360f8b19a364eae47c057ba678eb4cae9d98b0a4
bob <boblunt@live.cn>
2019-04-30 17:02:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-upload
+      action="https://jsonplaceholder.typicode.com/posts/"
+      list-type="picture-card"
+      :on-remove="handleRemove">
+      <i class="el-icon-plus"></i>
+    </el-upload>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { ElUploadInternalFileDetail } from 'element-ui/types/upload';
+
+export default Vue.extend({
+  name: 'app',
+  methods: {
+    handleRemove(file: ElUploadInternalFileDetail, fileList:File[]) {
+      console.log(file, fileList);
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
